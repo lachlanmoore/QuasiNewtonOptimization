cb48f59bff4d81628dbcf6a89471772e2bcaf7b6
Lachlan Moore <lachlanmoore@Lachlans-MacBook-Pro.local>
2020-12-13 14:55:51 -0500

    Comments

--- hartmann3.m
+++ hartmann3.m
@@ -1,4 +1,10 @@
-function [so] = hartmann3(x)
+function [y] = hartmann3(x)
+% [y] = hartmann3(x)
+%
+% 3 Hump Camel Function
+%
+% Lachlan Moore
+% 2020 December
 
 alpha = [1.0; 1.2; 3.0; 3.2];
 
@@ -7,20 +13,20 @@ A = [3, 10, 30;
      3, 10, 30;
      0.1, 10, 35];
  
-P = 10^(-4) * [3689, 1170, 2673;
+P = 1e-4 * [3689, 1170, 2673;
                4699, 4387, 7470;
                1091, 8732, 5547;
                381, 5743, 8828];
 
-so = 0;
+y = 0;
 for i = 1:4
    si = 0;
    for j = 1:3
        si = si + A(i,j)*(x(j) - P(i,j))^2;  
    end
    si = -1 * si;
-   so = so + alpha(i) * exp(si);
+   y = y + alpha(i) * exp(si);
 end
-so = -1 * so;
+y = -1 * y;
 
 end
